3f8b9450163215eb01e20968c8d12ca181ba75e5
ShanteTorres <kennedy2110@gmail.com>
2021-12-12 23:11:05 -0500

    commit tree bd04ad6d0c2405643c7211f4ab96fd0159c90fb7

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const arr1 = []
+    for (let i = 0; i < names.length; i++) {
+        const string = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        arr1.push(string)
+    }
+    return arr1 
+}
+
+function countDown(positiveNumber) {
+while (positiveNumber >= 0) {
+    console.log(positiveNumber);
+    positiveNumber--;
+    }
+}
